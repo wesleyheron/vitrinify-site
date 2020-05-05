b9af905564ba3c74cf78ad7ad72fceb2270e9327
Alisson Martineli <martineli.alisson@gmail.com>
2020-05-04 23:17:40 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Vitrinify (website)
+
+Site desenvolvido para o Mega Hack 2020 - 2ª edição (Equipe 6) (Desafio VTEX)
+
+https://www.megahack.com.br/
+
+## Install the dependencies
+
+```bash
+yarn
+```
+
+### Start the website in development mode
+
+```bash
+yarn start
+```
+
+### Build the website for production
+
+```bash
+yarn build
+```
---
+++ webpack.common.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([{ from: "./src/img", to: "./img" }]),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+      chunkFilename: "styles.css",
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
